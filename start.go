8fc24dbc29bc80c1964056e989f08ee7f4eb0a68
Coolfreewerx <78583528+Coolfreewerx@users.noreply.github.com>
2023-06-23 18:14:03 +0700

    add second edge many-to-many, add graph traversal

--- start.go
+++ start.go
@@ -25,6 +25,43 @@ func main() {
     }
 }
 
+func CreateCars(ctx context.Context, client *ent.Client) (*ent.User, error) {
+    // Create a new car with model "Tesla".
+    tesla, err := client.Car.
+        Create().
+        SetModel("Tesla").
+        SetRegisteredAt(time.Now()).
+        Save(ctx)
+    if err != nil {
+        return nil, fmt.Errorf("failed creating car: %w", err)
+    }
+    log.Println("car was created: ", tesla)
+
+    // Create a new car with model "Ford".
+    ford, err := client.Car.
+        Create().
+        SetModel("Ford").
+        SetRegisteredAt(time.Now()).
+        Save(ctx)
+    if err != nil {
+        return nil, fmt.Errorf("failed creating car: %w", err)
+    }
+    log.Println("car was created: ", ford)
+
+    // Create a new user, and add it the 2 cars.
+    a8m, err := client.User.
+        Create().
+        SetAge(30).
+        SetName("a8m").
+        AddCars(tesla, ford).
+        Save(ctx)
+    if err != nil {
+        return nil, fmt.Errorf("failed creating user: %w", err)
+    }
+    log.Println("user was created: ", a8m)
+    return a8m, nil
+}
+
 func CreateUser(ctx context.Context, client *ent.Client) (*ent.User, error) {
     u, err := client.User.
         Create().
@@ -69,43 +106,6 @@ func QueryCarUsers(ctx context.Context, a8m *ent.User) error {
     return nil
 }
 
-func CreateCars(ctx context.Context, client *ent.Client) (*ent.User, error) {
-    // Create a new car with model "Tesla".
-    tesla, err := client.Car.
-        Create().
-        SetModel("Tesla").
-        SetRegisteredAt(time.Now()).
-        Save(ctx)
-    if err != nil {
-        return nil, fmt.Errorf("failed creating car: %w", err)
-    }
-    log.Println("car was created: ", tesla)
-
-    // Create a new car with model "Ford".
-    ford, err := client.Car.
-        Create().
-        SetModel("Ford").
-        SetRegisteredAt(time.Now()).
-        Save(ctx)
-    if err != nil {
-        return nil, fmt.Errorf("failed creating car: %w", err)
-    }
-    log.Println("car was created: ", ford)
-
-    // Create a new user, and add it the 2 cars.
-    a8m, err := client.User.
-        Create().
-        SetAge(30).
-        SetName("a8m").
-        AddCars(tesla, ford).
-        Save(ctx)
-    if err != nil {
-        return nil, fmt.Errorf("failed creating user: %w", err)
-    }
-    log.Println("user was created: ", a8m)
-    return a8m, nil
-}
-
 func QueryCars(ctx context.Context, a8m *ent.User) error {
     cars, err := a8m.QueryCars().All(ctx)
     if err != nil {
@@ -121,5 +121,75 @@ func QueryCars(ctx context.Context, a8m *ent.User) error {
         return fmt.Errorf("failed querying user cars: %w", err)
     }
     log.Println(ford)
+    return nil
+}
+
+func CreateGraph(ctx context.Context, client *ent.Client) error {
+    // First, create the users.
+    a8m, err := client.User.
+        Create().
+        SetAge(30).
+        SetName("Ariel").
+        Save(ctx)
+    if err != nil {
+        return err
+    }
+    neta, err := client.User.
+        Create().
+        SetAge(28).
+        SetName("Neta").
+        Save(ctx)
+    if err != nil {
+        return err
+    }
+    // Then, create the cars, and attach them to the users created above.
+    err = client.Car.
+        Create().
+        SetModel("Tesla").
+        SetRegisteredAt(time.Now()).
+        // Attach this car to Ariel.
+        SetOwner(a8m).
+        Exec(ctx)
+    if err != nil {
+        return err
+    }
+    err = client.Car.
+        Create().
+        SetModel("Mazda").
+        SetRegisteredAt(time.Now()).
+        // Attach this car to Ariel.
+        SetOwner(a8m).
+        Exec(ctx)
+    if err != nil {
+        return err
+    }
+    err = client.Car.
+        Create().
+        SetModel("Ford").
+        SetRegisteredAt(time.Now()).
+        // Attach this car to Neta.
+        SetOwner(neta).
+        Exec(ctx)
+    if err != nil {
+        return err
+    }
+    // Create the groups, and add their users in the creation.
+    err = client.Group.
+        Create().
+        SetName("GitLab").
+        AddUsers(neta, a8m).
+        Exec(ctx)
+    if err != nil {
+        return err
+    }
+    err = client.Group.
+        Create().
+        SetName("GitHub").
+        AddUsers(a8m).
+        Exec(ctx)
+    if err != nil {
+        return err
+    }
+    log.Println("The graph was created successfully")
     return nil
 }
